037807864810d0172397f5cf27646dbd190c3851
ggtiger <wh123456>
2018-11-14 15:40:39 +0800

    webpack 图片地址问题恢复，暂时不处理

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -44,8 +44,7 @@ module.exports = {
                 loader: 'url-loader',
                 options: {
                     limit: 10000,
-                    name: utils.assetsPath('img/[name].[hash:7].[ext]'),
-                    publicPath: './img'
+                    name: utils.assetsPath('img/[name].[hash:7].[ext]')
                 }
             },
             {
