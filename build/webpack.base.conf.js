93adee5aa1694fe5001f4ee90ddec667c8d9af2e
ggtiger <wh123456>
2018-11-14 15:36:48 +0800

    字体打包webpack修改

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -43,7 +43,7 @@ module.exports = {
                 test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
                 loader: 'url-loader',
                 options: {
-                    limit: 100000,
+                    limit: 10000,
                     name: utils.assetsPath('img/[name].[hash:7].[ext]'),
                     publicPath: './img'
                 }
@@ -61,7 +61,7 @@ module.exports = {
                 loader: 'url-loader',
                 options: {
                     limit: 100000,
-                    name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+                    name: utils.assetsPath('fonts/[name].[hash:7].[ext]'),
                 }
             }
         ]
