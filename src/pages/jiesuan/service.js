91c46b75b1d85c41f02d35bc4ec1e312eecd8b41
orangeStrom <313066000@qq.com>
2018-11-27 09:27:54 +0800

    结算单修改

--- src/pages/jiesuan/service.js
+++ src/pages/jiesuan/service.js
@@ -1,9 +1,8 @@
 import db from "@/api/db";
 import Store from "@/store"
-const doCall = function(param) {
-    const api = { "namespace": "GJ.EBZ.RV", "class": "FDBK", "method": "DoExecute", params: [param] };
-    return db.call(api);
-}
+import { setDB, doOpen, doDelete, doSave, doCheck, doReCheck } from "rs-vcore/service/Service01";
+setDB(db);
+
 const getAddData = function(param) {
     param["TYPE"] = "addData";
     return db.openTables([
@@ -32,14 +31,42 @@ const getAddData = function(param) {
     ])
 }
 
-const saveData = function(param) {
-    param["TYPE"] = "save";
-    return doCall(param);
+const _getSTLFMITEMPara = function(STLTYPEID,BILLDATE) {
+    return {
+        modalName: "TBV_STLFMITEM",
+        where: `[STLFMID] = (SELECT STLFMID FROM VBV_STLFM_REF WHERE  STLTYPEID ='${STLTYPEID}' AND TO_DATE('${BILLDATE}','YYYY-MM-DD') BETWEEN SDATE AND NVL (EDATE,TO_DATE('2099-12-31','YYYY-MM-DD')))`,
+        orderBy: "[ENTRYNUM]",
+        pageSize: 1,
+        pageIndex: 1
+    }
 }
 
-const openData = function(param) {
-    param["TYPE"] = "loadData";
-    return doCall(param);
+const _getCOPYDTSPara = function(DSNODEID, STLFMID) {
+    return {
+        modalName: "TBV_SNSTLDTS_M",
+        where: `[BILLID] IN (SELECT * FROM (SELECT BILLID FROM TBV_SNSTL WHERE AID = @AID AND SNODEID = '${DSNODEID}' AND STLFMID = '${STLFMID}' AND NVL(ISDEL,0) = 0  ORDER BY  BILLDATE,FHOUR,FMINUTE DESC) WHERE  ROWNUM <2  )`,
+        orderBy: "",
+        pageSize: 1,
+        pageIndex: 1
+    }
 }
 
-export { getAddData, saveData, openData }
+const doLoadCOPYDTS = async function({ DSNODEID, STLFMID }) {
+    return db.open(_getCOPYDTSPara(DSNODEID, STLFMID))
+}
+
+const doLoadSTLFMITE = async function({ STLTYPEID }) {
+    return db.open(_getSTLFMITEMPara(STLTYPEID))
+}
+
+const doLoadSnode = async function({ SNODEID }) {
+    return db.open({
+        modalName: "TBV_CHAINSND_SEL",
+        where: `[SNODEID] ='${SNODEID}'`,
+        orderBy: "",
+        pageSize: 1,
+        pageIndex: 1
+    })
+}
+
+export default { doLoadCOPYDTS, doLoadSTLFMITE, doLoadSnode, doOpen, doSave, doDelete, doCheck, doReCheck }
