faeb96f356abddb02cc888016bc5bc734836830a
renren1993 <1094437360@qq.com>
2018-11-27 17:24:51 +0800

    2

--- src/pages/main/Index.vue
+++ src/pages/main/Index.vue
@@ -4,16 +4,16 @@
       <router-view ref="view" />
     </keep-alive>
     <rs-tabbar v-model="selected">
-      <rs-tab-item id="main1" icon="mui-icon rr-font rr-gongzuotai">
+      <rs-tab-item id="main1" icon="mui-icon rr-font rr-gongzuotai f24">
         <span slot="label">工作台</span>
       </rs-tab-item>
-      <rs-tab-item id="main2" icon="mui-icon rr-font rr-zhishiku">
+      <rs-tab-item id="main2" icon="mui-icon rr-font rr-zhishiku f24">
         <span slot="label">知识库</span>
       </rs-tab-item>
-      <rs-tab-item id="main3" icon="mui-icon rr-font rr-xiaoxi">
+      <rs-tab-item id="main3" icon="mui-icon rr-font rr-xiaoxi f24">
         <span slot="label">消息</span>
       </rs-tab-item>
-      <rs-tab-item id="main4" icon="mui-icon rr-font rr-wode">
+      <rs-tab-item id="main4" icon="mui-icon rr-font rr-wode f24">
         <span slot="label">我的</span>
       </rs-tab-item>
     </rs-tabbar>
