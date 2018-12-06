e034534e091d4704f4386ee78123dbf361b80a50
orangeStrom <313066000@qq.com>
2018-12-06 11:20:05 +0800

    结算动态表

---
+++ src/pages/jiesuandongtai/main/Index.vue
@@ -0,0 +1,61 @@
+<template>
+  <div style="height:100%">
+    <transition :name="transitionName" @enter="enter">
+      <keep-alive>
+        <router-view></router-view>
+      </keep-alive>
+    </transition>
+  </div>
+</template>
+<script>
+import store from "./store";
+import { debug } from "util";
+export default {
+  data() {
+    return {
+      transitionName: "slide-right"
+    };
+  },
+  activated: function() {
+    console.log(this.$route.path, "color:red");
+    setTimeout(async () => {
+      this.$store.commit("jiesuandongtai/setParams", this.$route.query);
+      if ("ADD" == this.$route.query.ACTION) {
+        
+      }
+      if ("VIEW" == this.$route.query.ACTION) {
+        this.$store.commit("jiesuandongtai/addDefault");
+        this.$store.dispatch("jiesuandongtai/openReport", this.$route.query);
+      }
+    }, 600);
+  }
+};
+</script>
+<style>
+.slide-right-enter-active,
+.slide-right-leave-active,
+.slide-left-enter-active,
+.slide-left-leave-active {
+  will-change: transform;
+  transition: all 500ms;
+  position: absolute;
+}
+.slide-right-enter {
+  opacity: 0;
+  transform: translate3d(-100%, 0, 0);
+}
+.slide-right-leave-active {
+  opacity: 0;
+  transform: translate3d(100%, 0, 0);
+}
+.slide-left-enter {
+  opacity: 0;
+  transform: translate3d(100%, 0, 0);
+}
+.slide-left-leave-active {
+  opacity: 0;
+  transform: translate3d(-100%, 0, 0);
+}
+</style>
+
+
---
+++ src/pages/jiesuandongtai/router.js
@@ -0,0 +1,33 @@
+export default {
+    path: '/jiesuandongtai',
+    name: 'jiesuandongtai',
+    component: r => require.ensure([], () => r(require('@/pages/jiesuandongtai/main/index')), "jiesuandongtai" /*按需加载，名称一样打包在一个文件*/ ),
+    redirect: '/jiesuandongtai/main/jiesuandongtai',
+    children: [ 
+        {
+            path: '/jiesuandongtai/main/jiesuandongtai',
+            name: "/jiesuandongtai/main/jiesuandongtai",
+            component: r => require.ensure([], () => r(require('@/pages/jiesuandongtai/main/views/jiesuandongtai')), "jiesuandongtai"),
+            props: { TITLE: '结算日报' },
+            meta: {
+                keepAlive: true
+            }
+        }, {
+            path: '/jiesuandongtai/main/snodesel',
+            name: "/jiesuandongtai/main/snodesel",
+            component: r => require.ensure([], () => r(require('@/pages/com/views/snode_sel')), "jiesuandongtai"),
+            props: { TITLE: '选择经营门店', refStore: { mutation: "jiesuandongtai/setSnode", path: "QRYADV" } },
+            meta: {
+                keepAlive: true
+            }
+        },{
+            path: '/jiesuandongtai/main/detail',
+            name: "/jiesuandongtai/main/detail",
+            component: r => require.ensure([], () => r(require('@/pages/jiesuandongtai/main/views/detail')), "jiesuandongtai"),
+            props: { TITLE: '明细' },
+            meta: {
+                keepAlive: true
+            }
+        }
+    ]
+}
