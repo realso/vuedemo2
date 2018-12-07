3d789961c0e8606535cf999be09ca36ceb760ac4
renren1993 <1094437360@qq.com>
2018-12-07 15:14:27 +0800

    Merge branch 'master' of https://github.com/realso/vuedemo2

--- src/App.vue
+++ src/App.vue
@@ -15,5 +15,6 @@ export default {
 <style>
 #app {
   height: 100%;
+  width: 100%;
 }
 </style>
--- src/pages/jiesuan/add/Index.vue
+++ src/pages/jiesuan/add/Index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div style="height:100%">
+  <div style="height:100%;width: 100%;">
     <transition :name="transitionName">
       <keep-alive>
         <router-view></router-view>
